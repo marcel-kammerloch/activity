e894538bf709fca352e4abea45693237831dd3d4
Marcel K <developer.marcel1@gmail.com>
2025-06-03 12:56:31 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import base64
 import os
 from dotenv import load_dotenv
 import sys
+import time
 
 # Load .env file
 load_dotenv()
@@ -10,7 +11,7 @@ load_dotenv()
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 REPO_OWNER = "marcel-kammerloch"
 REPO_NAME = "activity"
-FILE_PATH = "activity/file.txt"
+FILE_PATH = "file.txt"
 BRANCH = "main"
 
 GITHUB_API_URL = f"https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/contents/{FILE_PATH}"
@@ -20,29 +21,35 @@ GITHUB_API_HEADERS = {
     "Accept": "application/vnd.github+json"
 }
 
-def readFile():
-    with open(FILE_PATH, 'r') as currentFile:
-        content = currentFile.read()
-    return content
+# def readFile():
+#     with open(FILE_PATH, 'r') as currentFile:
+#         content = currentFile.read()
+#     return content
 
-def updateFile(newContent):
-    with open(FILE_PATH, 'w') as file:
-        file.write(newContent)
+# def updateFile(newContent):
+#     with open(FILE_PATH, 'w') as file:
+#         file.write(newContent)
 
-def incrementNumberOfFile():
-    currentNumber = int(readFile())
+# def incrementNumberOfFile():
+#     currentNumber = int(readFile())
 
-    newNumber = str(currentNumber + 1)
+#     newNumber = str(currentNumber + 1)
 
-    updateFile(newNumber)
+#     updateFile(newNumber)
 
-    return newNumber
+#     return newNumber
 
 def getFileSha(debug=False):
     response = requests.get(GITHUB_API_URL, headers=GITHUB_API_HEADERS)
     status = response.status_code
+    json = response.json()
 
-    return  str(response.json()["sha"]) if status == 200 else (status if debug is True else None)
+    if status == 200:
+        return str(response.json()["sha"]) 
+    elif debug is True: 
+        return { status, json }
+    else:
+        return None
 
 def updateFileOnGithub(content, fileSha):
     encodedContent = base64.b64encode(content.encode()).decode()
@@ -56,15 +63,17 @@ def updateFileOnGithub(content, fileSha):
         data["sha"] = fileSha
 
     response = requests.put(GITHUB_API_URL, headers=GITHUB_API_HEADERS, json=data)
+    status = response.status_code
 
-    if response.status_code == 200:
+    if (status == 200) or (status == 201):
         print("File successfully updated")
     else: 
-        print(f"Error: {response.status_code}, {response.json()}")
+        print(f"Error: {status}, {response.json()}")
 
 def main():
     try:
-        newContent = incrementNumberOfFile()
+        newContent =  str(int(time.time() * 1000))
+
         fileSha = getFileSha()
 
         updateFileOnGithub(newContent, fileSha)
@@ -78,17 +87,17 @@ def __test__():
         print(f".env file {"NOT " if GITHUB_TOKEN is None else "successfully"} loaded")
 
         # Test reading file
-        content = readFile()
-        if(content is None): raise Exception("Content of File is None")
+        # content = readFile()
+        # if(content is None): raise Exception("Content of File is None")
 
         # Test if github api is working
-        fileSha = getFileSha(True)
-        if(int.is_integer(fileSha)): raise Exception(f"fileSha is not a string. Status: {fileSha}")
+        fileSha = getFileSha(debug=True)
+        if(isinstance(fileSha, (int, float))): raise Exception(f"fileSha is not a string. Error: {fileSha}")
     except Exception as e:
         print(f"Error: {e}")
 
 if(__name__ == "__main__"):
-    if (len(sys.argv) > 1) and ((sys.argv[1] == "-t" )or (sys.argv[1] == "--test")):
+    if (len(sys.argv) > 1) and ((sys.argv[1] == "-t" ) or (sys.argv[1] == "--test")):
         print("Testing...")
         __test__()
     else:
